b72ddca24dd787bae16933941d2e64448c5c6009
= <=>
2022-05-12 13:46:42 -0300

    send and receive messages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,29 @@ import styles from "./home.module.scss";
 import { io, Socket } from "socket.io-client";
 import { useEffect, useState } from "react";
 
-import { getSession, signIn, signOut, useSession } from "next-auth/react";
+import { getSession, signOut, useSession } from "next-auth/react";
 import { GetServerSideProps } from "next";
+import { FormEvent } from "react";
+
+interface Message {
+  user: {
+    name: string;
+    email: string;
+    image: string;
+  };
+  text: string;
+}
 
 export default function Home() {
   const { data: session, status } = useSession();
   const [socket, setSocket] = useState<Socket>(null);
+  const [message, setMessage] = useState("");
+
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    console.log(messages);
+  }, [messages]);
 
   useEffect(() => {
     const newSocket = io("http://localhost:8080");
@@ -18,17 +35,38 @@ export default function Home() {
     };
   }, [setSocket]);
 
-  if (socket) {
-    socket.on("enter", (msg) => {
-      console.log(msg);
-    });
-    socket.on("exit", (msg) => {
-      console.log(msg);
-    });
-  }
+  useEffect(() => {
+    if (socket) {
+      socket.on("receive_message", (msg: Message) => {
+        console.log(`${msg.user.name}: ${msg.text}`);
+      });
+    }
+  }, [socket]);
 
   if (status === "loading") {
     return <span>Fazendo login...</span>;
+  }
+
+  function sendMessage(e: FormEvent<HTMLFormElement>, text: string) {
+    e.preventDefault();
+
+    socket.emit("send_message", {
+      user: session.user,
+      text,
+    });
+
+    setMessage("");
+    setMessages([
+      ...messages,
+      {
+        user: {
+          name: session.user.name,
+          email: session.user.email,
+          image: session.user.image,
+        },
+        text,
+      },
+    ]);
   }
 
   return (
@@ -36,6 +74,25 @@ export default function Home() {
       <h1>Hello World!</h1>
       Signed in as {session.user.email} <br />
       <button onClick={() => signOut()}>Sign out</button>
+      <form onSubmit={(e) => sendMessage(e, message)}>
+        <input
+          type="text"
+          required
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+      <div className="messages">
+        {messages.map((msg) => {
+          return (
+            <span style={{ display: "block" }}>
+              <strong>{msg.user.name} </strong>
+              {msg.text}
+            </span>
+          );
+        })}
+      </div>
     </div>
   );
 }
