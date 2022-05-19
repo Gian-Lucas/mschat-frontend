528a5305e721742a42d93426e323de826c401531
= <=>
2022-05-19 14:58:41 -0300

    remove color mode

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,6 @@
 import { extendTheme } from "@chakra-ui/react";
 
-const config = {
-  initialColorMode: "dark",
-  useSystemColorMode: false,
-};
-
 export const theme = extendTheme({
-  config,
   fonts: {
     heading: "Roboto",
     body: "Roboto",
